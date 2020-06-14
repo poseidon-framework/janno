af1a8908e5f2dd58f51a5e5ce5ab285f4f0c29de
Clemens Schmid <clemens@nevrome.de>
2020-06-14 14:13:22 +0200

    made age string parser work

--- R/a2j.R
+++ R/a2j.R
@@ -15,18 +15,20 @@ a2j <- function(
     Individual_ID = rep(NA, nrow(anno)),
     Collection_ID = NA,
     Skeletal_Element = NA,
+    # spatial location
     Country = NA,
     Location = NA,
     Site = NA,
     Latitude = NA,
     Longitude = NA,
+    # temporal location
     Date_C14_Labnr = NA,
     Date_C14_Uncal_BP = NA,
     Date_C14_Uncal_Dev = NA,
-    Date_BC_Median = NA,
     Date_BC_Start = NA,
     Date_BC_Stop = NA,
     Date_Type = NA,
+    Date_BC_Simple_Mean = NA,
     No_of_Libraries = NA,
     Data_Type = NA,
     Genotype_Ploidy = NA,
@@ -49,24 +51,24 @@ a2j <- function(
     Note = NA
   )
   
-  janno$Individual_ID = anno %c% "instance_id"
-  janno$Collection_ID = anno %c% "master_id"
-  janno$Skeletal_Element = anno %c% "skeletal_element"
-  janno$Country = anno %c% "country"
-  janno$Location = anno %c% "location"
-  janno$Site = anno %c% "site"
-  janno$Latitude = anno %cr% "lat"
-  janno$Longitude = anno %cr% "long"
+  janno$Individual_ID <- anno %c% "instance_id"
+  janno$Collection_ID <- anno %c% "master_id"
+  janno$Skeletal_Element <- anno %c% "skeletal_element"
+  janno$Country <- anno %c% "country"
+  janno$Location <- anno %c% "location"
+  janno$Site <- anno %c% "site"
+  janno$Latitude <- anno %cr% "lat"
+  janno$Longitude <- anno %cr% "long"
   
+  # splitting age string
   age_table <- split_age_string(anno[["date_one_of_two_formats_format_1_95_4_percent_ci_calibrated_radiocarbon_age_conventional_radiocarbon_age_b_lab_number_e_g_5983_5747_cal_bce_6980_50_b_b_beta_226472_format_2_archaeological_context_date_b_e_g_2500_1700_bce"]])
-  
-  janno$Date_C14_Labnr = age_table$age_c14_labnr
-  janno$Date_C14_Uncal_BP = age_table$age_c14_uncal_BP
-  janno$Date_C14_Uncal_Dev = age_table$age_c14_uncal_BP_dev
-  janno$Date_BC_Median = NA
-  janno$Date_BC_Start = NA
-  janno$Date_BC_Stop = NA
-  janno$Date_Type = NA
+  janno$Date_C14_Labnr <- age_table$Date_C14_Labnr
+  janno$Date_C14_Uncal_BP <- age_table$Date_C14_Uncal_BP
+  janno$Date_C14_Uncal_BP_Dev <- age_table$Date_C14_Uncal_BP_Dev
+  janno$Date_BC_Start <- age_table$Date_BC_Start
+  janno$Date_BC_Stop <- age_table$Date_BC_Stop
+  janno$Date_Type <- age_table$Date_Type
+  janno$Date_BC_Simple_Mean <- (janno$Date_BC_Start + janno$Date_BC_Stop)/2
    
   
   janno <- anno
@@ -87,75 +89,87 @@ a2j <- function(
 split_age_string <- function(x) {
   
   res <- tibble::tibble(
-    age_arch_start_BC = rep(NA, length(x)),
-    age_arch_stop_BC = NA,
-    age_c14_uncal_BP = NA,
-    age_c14_uncal_BP_dev = NA,
-    age_c14_labnr = NA
+    x = x,
+    Date_C14_Labnr = rep(NA, length(x)),
+    Date_C14_Uncal_BP = NA,
+    Date_C14_Uncal_BP_Dev = NA,
+    Date_BC_Start = NA,
+    Date_BC_Stop = NA,
+    Date_Type = NA
   )
   
-  # c14 age
-  c14_age_ids <- grep("\\(", x)
-  res$age_c14_labnr[c14_age_ids] <- stringr::str_extract_all(x[c14_age_ids], "[A-Z,a-z]{2,7}-[0-9]*") %>% sapply(., function(y) { paste(y, collapse = ";") } )
+  # determine type of date info
+  none_ids <- which(is.na(x))
+  present_ids <- grep("present", x)
+  c14_age_ids <- grep("cal", x)
+  res$Date_Type[c14_age_ids] <- "C14"
+  res$Date_Type[-c14_age_ids] <- "contextual"
+  res$Date_Type[present_ids] <- "modern"
+  res$Date_Type[none_ids] <- "none"
+  
+  # parse uncalibrated c14 age info
+  res$Date_C14_Labnr[c14_age_ids] <- stringr::str_extract_all(x[c14_age_ids], "[A-Z,a-z]{2,7}-[0-9]*") %>% sapply(., function(y) { paste(y, collapse = ";") } )
   uncal_dates <- stringr::str_extract_all(x[c14_age_ids], "[0-9]{1,5}±[0-9]{1,4}")
-  res$age_c14_uncal_BP[c14_age_ids] <- sapply(uncal_dates, function(z) { 
+  res$Date_C14_Uncal_BP[c14_age_ids] <- sapply(uncal_dates, function(z) { 
     sapply(strsplit(z, "±"), function(a) { a[1] } ) %>% 
       paste(collapse = ";") 
   } )
-  res$age_c14_uncal_BP_dev[c14_age_ids] <- sapply(uncal_dates, function(z) { 
+  res$Date_C14_Uncal_BP_Dev[c14_age_ids] <- sapply(uncal_dates, function(z) { 
     sapply(strsplit(z, "±"), function(a) { a[2] } ) %>% 
       paste(collapse = ";") 
   } )
   
-  # arch age
-  arch_age_split <- x[-c14_age_ids] %>% strsplit("-|\\ ") %>% lapply(function(y) {y[y != ""]})
-  stop <- start <- rep(NA, length(arch_age_split))
-  for (i in 1:length(arch_age_split)) {
-    if (is.na(arch_age_split[[i]][1])) {
+  # parse simplified start and stop age
+  simple_age_split <- x %>% strsplit("-|\\ ") %>% lapply(function(y) {y[y != ""]})
+  stop <- start <- rep(NA, length(simple_age_split))
+  for (i in 1:length(simple_age_split)) {
+    # no age info
+    if (is.na(simple_age_split[[i]][1])) {
       start[i] <- NA
       stop[i] <- NA
       next
     }
-    if (arch_age_split[[i]][1] == "present") {
-      start[i] <- 2000
-      stop[i] <- 2000
-      next
-    }
-    if (length(arch_age_split[[i]]) == 2) {
-      if (arch_age_split[[i]][2] == "BCE") {
-        start[i] <- -as.numeric(arch_age_split[[i]][1])
-        stop[i] <- -as.numeric(arch_age_split[[i]][1])
+    # no range: only one value e.g. 5000 BCE
+    if (length(simple_age_split[[i]]) == 2) {
+      if (simple_age_split[[i]][2] == "BCE") {
+        start[i] <- -as.numeric(simple_age_split[[i]][1])
+        stop[i] <- -as.numeric(simple_age_split[[i]][1])
         next
       }
-      if (arch_age_split[[i]][2] == "CE") {
-        start[i] <- as.numeric(arch_age_split[[i]][1])
-        stop[i] <- as.numeric(arch_age_split[[i]][1])
+      if (simple_age_split[[i]][2] == "CE") {
+        start[i] <- as.numeric(simple_age_split[[i]][1])
+        stop[i] <- as.numeric(simple_age_split[[i]][1])
         next
       } 
-      if (all(grepl("^[0-9]+$", arch_age_split[[i]]))) {
-        start[i] <- -as.numeric(arch_age_split[[i]][1])
-        stop[i] <- -as.numeric(arch_age_split[[i]][2])
+      if (all(grepl("^[0-9]+$", simple_age_split[[i]]))) {
+        start[i] <- -as.numeric(simple_age_split[[i]][1])
+        stop[i] <- -as.numeric(simple_age_split[[i]][2])
         next
       }
     }
-    if (arch_age_split[[i]][3] == "BCE") {
-      start[i] <- -as.numeric(arch_age_split[[i]][1])
-      stop[i] <- -as.numeric(arch_age_split[[i]][2])
+    # normal range 5000-4700 BCE
+    if (simple_age_split[[i]][3] %in% c("BCE", "calBCE")) {
+      start[i] <- -as.numeric(simple_age_split[[i]][1])
+      stop[i] <- -as.numeric(simple_age_split[[i]][2])
       next
     }
-    if (arch_age_split[[i]][3] == "CE") {
-      start[i] <- as.numeric(arch_age_split[[i]][1])
-      stop[i] <- as.numeric(arch_age_split[[i]][2])
+    if (simple_age_split[[i]][3] %in% c("CE", "calCE")) {
+      start[i] <- as.numeric(simple_age_split[[i]][1])
+      stop[i] <- as.numeric(simple_age_split[[i]][2])
       next
     }
-    if (arch_age_split[[i]][2] == "BCE" & arch_age_split[[i]][4] == "CE") {
-      start[i] <- -as.numeric(arch_age_split[[i]][1])
-      stop[i] <- as.numeric(arch_age_split[[i]][3])
+    if (simple_age_split[[i]][2] %in% c("BCE", "calBCE") & simple_age_split[[i]][4] %in% c("CE", "calCE")) {
+      start[i] <- -as.numeric(simple_age_split[[i]][1])
+      stop[i] <- as.numeric(simple_age_split[[i]][3])
       next
     }
   }
-  res$age_arch_start_BC[-c14_age_ids] <- start
-  res$age_arch_stop_BC[-c14_age_ids] <- stop
+  
+  res$Date_BC_Start <- start
+  res$Date_BC_Stop <- stop
+  
+  # replace all empty values with NA
+  res[res == ""] <- NA
   
   return(res)
 }
