7774e41c20e504ed4d006796eacfeb980ab4529c
Clemens Schmid <clemens@nevrome.de>
2020-07-06 20:21:58 +0200

    setup for singularity

---
+++ singularity/build_sif.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm poseidon2.sif
+
+sudo singularity build poseidon2.sif poseidon2.def
---
+++ singularity/poseidon2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+singularity exec /projects/poseidon/anno2janno/singularity/poseidon2.sif /poseidon2.R "$@"
+
