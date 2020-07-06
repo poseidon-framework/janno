cfe4f4a6d8da79b61bb44a7a4a19e8f2b36fdea1
Clemens Schmid <clemens@nevrome.de>
2020-07-06 15:51:54 +0200

    and more work on the merge module

--- R/merge_module.R
+++ R/merge_module.R
@@ -1,33 +1,70 @@
 merge_module <- function(input_file, output_directory, log_directory) {
   
+  merge_start_message(input_file, output_directory, log_directory)
+
   list_of_packages <- merge_read_package_list(input_file)
   
-  merge_start_message(input_file, output_directory, log_directory)
   merge_print_packages(list_of_packages)
   
   #validate_module(list_of_packages)
 
+  create_output_directory()
+  
+  merge_concat_janno_files(list_of_packages, output_directory)
+  
   plink_merge_file <- merge_create_plink_merge_input_file(list_of_packages, log_directory)
   
+  plink_order_file <- merge_create_order_file_from_fam_files(list_of_packages, log_directory)
   
-    
+  plink_merge(plink_merge_file, plink_order_file, output_directory, "poseidon2_merged", log_directory)
+
 }
 
-plink_merge <- function() {
-  
+plink_merge(plink_merge_file, plink_order_file, output_directory, output_files_name, log_directory) {
+  cat("Merge genome data with plink...\n=> ")
+  system(paste(
+    'sbatch -p "short" -c 4 --mem=10000 -J "poseidon2_merge"',
+    '-o', file.path(log_directory, 'poseidon2_%j.out'),
+    '-e', file.path(log_directory, 'poseidon2_%j.err'),
+    '--wrap= "plink',
+      '--merge-list', plink_merge_file,
+      '--make-bed', 
+      '--indiv-sort', plink_order_file,
+      '--keep-allele-order',
+      '--out', file.path(output_directory, output_files_name),
+    '&& mv', paste0(file.path(output_directory, output_files_name), '.log'), file.path(log_directory, 'plink.log')
+  ))
 }
 
-merge_create_order_file_from_fam_files <- function() {
+merge_create_order_file_from_fam_files <- function(list_of_packages, log_directory) {
+  cat("Merge fam files to get order file...\n")
+  list_of_fam_files <- list.files(list_of_packages, pattern = ".fam", full.names = T)
+  list_of_fam_tables <- lapply(list_of_fam_files, function(fam) {
+    suppressMessages(readr::read_delim(fam, delim = " ", col_names = F))
+  })
+  concat_first_two_columns <- do.call(rbind, list_of_fam_tables)[,1:2]
+  plink_order_file <- file.path(output_directory, "poseidon2_merge_plink_order_file.txt")
+  readr::write_tsv(concat_first_two_columns, path = plink_order_file, col_names = FALSE)
+  return(plink_order_file)
+}
+
+merge_concat_janno_files <- function(list_of_packages, output_directory) {
+  cat("Merge janno files...\n")
+  list_of_janno_files <- list.files(list_of_packages, pattern = ".janno", full.names = T)
+  list_of_janno_tables <- lapply(list_of_janno_files, function(janno) {
+    suppressMessages(readr::read_tsv(janno))
+  })
+  new_janno <- do.call(rbind, list_of_janno_tables)
+  readr::write_tsv(new_janno, path = file.path(output_directory, "hu.janno"))
 }
 
 merge_create_plink_merge_input_file <- function(list_of_packages, log_directory) {
   cat("Creating input file for plink merge...\n")
   list_of_file_tripels <- sapply(list_of_packages, function(package) {
-    file_list <- list.files(package, full.names = T)
-    relevant_files <- grep(".bed|.bim|.fam", file_list, value = T)
-    paste(relevant_files, collapse = " ")
+    file_list <- list.files(package, pattern = ".bed|.bim|.fam", full.names = T)
+    paste(file_list, collapse = " ")
   }, USE.NAMES = F)
-  plink_merge_file <- file.path(log_directory, "merge_list_file")
+  plink_merge_file <- file.path(log_directory, "poseidon2_merge_plink_input_file.txt")
   writeLines(list_of_file_tripels, con = plink_merge_file)
   return(plink_merge_file)
 }
