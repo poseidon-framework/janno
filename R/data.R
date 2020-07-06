3075f8b4813da1b76002a971894b77c535e35da1
Clemens Schmid <clemens@nevrome.de>
2020-07-06 20:42:21 +0200

    cleaning meta data

--- R/data.R
+++
@@ -1,9 +0,0 @@
-#' title
-#'
-#' description
-#'
-#' @format A data.frame
-#'
-#'
-#' @name colname_reference
-NULL
--- data-raw/data_prep.R
+++
@@ -1,3 +0,0 @@
-colname_reference <- readr::read_csv("data-raw/anno_janno_variable_name_reference.csv")
-
-usethis::use_data(colname_reference)
