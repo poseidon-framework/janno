7fe23dc2f276d84885228d42f286ad7745cb0643
Clemens Schmid <clemens@nevrome.de>
2020-07-17 11:57:25 +0200

    more .yml file validation

--- R/validate_POSEIDON_yml.R
+++ R/validate_POSEIDON_yml.R
@@ -12,18 +12,43 @@ can_POSEIDON_yml_be_read <- function(x) {
 validate_POSEIDON_yml <- function(x) {
   return(
     is_valid_poseidon_version(x$poseidonVersion) &
-      is_valid_string(x$title) &
-      is_valid_string(x$contributor$name) &
-      is_valid_email(x$contributor$email)
+    is_valid_string(x$title) &
+    is_valid_string(x$contributor$name) &
+    is_valid_email(x$contributor$email) &
+    is_valid_date(x$lastModified) &
+    is_valid_string(x$bibFile) &
+    is_valid_string(x$genotypeData$format) &
+    is_valid_string(x$genotypeData$genoFile) &
+    is_valid_string(x$genotypeData$snpFile) &
+    is_valid_string(x$genotypeData$indFile) &
+    is_valid_string(x$jannoFile)
   )
 }
 
 is_valid_email <- function(x) {
-  check <- grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x), ignore.case = TRUE)
-  if ( !check ) {
-    cli::cli_alert_danger("Not a valid email adress for the contributor")
+  check_0 <- is_valid_string(x)
+  if ( !check_0 ) {
+    return(check_0)
+  } else {
+    check_2 <- grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x), ignore.case = TRUE)
+    if ( !check_2 ) {
+      cli::cli_alert_danger("Not a valid email adress for the contributor")
+    }
+    return(check_2)
   }
-  return(check)
+}
+
+is_valid_date <- function(x) {
+  check_0 <- is_valid_string(x)
+  if ( !check_0 ) {
+    return(check_0)
+  } else {
+    check_2 <- !is.na(suppressWarnings(as.POSIXlt(x, format = "%Y-%m-%d")))
+    if ( !check_2 ) {
+      cli::cli_alert_danger("Not a valid email adress for the contributor")
+    }
+    return(check_2)
+  }
 }
 
 is_valid_poseidon_version <- function(x) {
