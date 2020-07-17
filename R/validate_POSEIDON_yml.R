4f2701eec44cd6317727a48e255956154e57ebd9
Clemens Schmid <clemens@nevrome.de>
2020-07-17 14:19:32 +0200

    update of .yml validator to use the new features

--- R/validate_POSEIDON_yml.R
+++ R/validate_POSEIDON_yml.R
@@ -11,57 +11,54 @@ can_POSEIDON_yml_be_read <- function(x) {
 
 validate_POSEIDON_yml <- function(x) {
   return(
-    is_valid_poseidon_version(x$poseidonVersion) &
-    is_valid_string(x$title) &
-    is_valid_string(x$contributor$name) &
-    is_valid_email(x$contributor$email) &
-    is_valid_date(x$lastModified) &
-    is_valid_string(x$bibFile) &
-    is_valid_string(x$genotypeData$format) &
-    is_valid_string(x$genotypeData$genoFile) &
-    is_valid_string(x$genotypeData$snpFile) &
-    is_valid_string(x$genotypeData$indFile) &
-    is_valid_string(x$jannoFile)
+    positioned_feedback(x$poseidonVersion, is_valid_poseidon_version, "poseidonVersion") &
+    positioned_feedback(x$title, is_valid_string, "title") &
+    positioned_feedback(x$contributor$name, is_valid_string, "contributor > name") &
+    positioned_feedback(x$contributor$email, is_valid_email, "contributor > email") &
+    positioned_feedback(x$lastModified, is_valid_date, "lastModified") &
+    positioned_feedback(x$bibFile, is_valid_string, "bibFile") &
+    positioned_feedback(x$genotypeData$format, is_valid_string, "genotypeData > format") &
+    positioned_feedback(x$genotypeData$genoFile, is_valid_string, "genotypeData > genoFile") &
+    positioned_feedback(x$genotypeData$snpFile, is_valid_string, "genotypeData > snpFile") &
+    positioned_feedback(x$genotypeData$indFile, is_valid_string, "genotypeData > indFile") &
+    positioned_feedback(x$jannoFile, is_valid_string, "jannoFile")
   )
 }
 
 is_valid_email <- function(x) {
   check_0 <- is_valid_string(x)
-  if ( !check_0 ) {
-    return(check_0)
-  } else {
-    check_2 <- grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x), ignore.case = TRUE)
-    if ( !check_2 ) {
+  if ( check_0 ) {
+    check_1 <- grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>", as.character(x), ignore.case = TRUE)
+    if ( !check_1 ) {
       cli::cli_alert_danger("Not a valid email adress for the contributor")
+      return(check_1)
     }
-    return(check_2)
   }
+  return(check_0)
 }
 
 is_valid_date <- function(x) {
   check_0 <- is_valid_string(x)
-  if ( !check_0 ) {
-    return(check_0)
-  } else {
+  if ( check_0 ) {
     check_1 <- !is.na(suppressWarnings(as.POSIXlt(x, format = "%Y-%m-%d")))
     if ( !check_1 ) {
       cli::cli_alert_danger("Not a valid date of format %Y-%m-%d")
+      return(check_1)
     }
-    return(check_1)
   }
+  return(check_0)
 }
 
 is_valid_poseidon_version <- function(x) {
   check_0 <- is_valid_string(x)
-  if ( !check_0 ) {
-    return(check_0)
-  } else {
+  if ( check_0 ) {
     check_1 <- grepl("[2]{1}\\.[0-9]+\\.[0-9]+", x)
     if ( !check_1 ) {
       cli::cli_alert_danger("Not a valid POSEIDON v.2 version number")
+      return(check_1)
     }
-    return(check_1)
   }
+  return(check_0)
 }
 
 has_POSEIDON_yml_the_necessary_elements <- function(
