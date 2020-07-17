0ced09ea659f0c99dda553d6a02297b2f6a42906
Clemens Schmid <clemens@nevrome.de>
2020-07-17 11:12:15 +0200

    added deployment to build script

--- singularity/build_sif.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm poseidon2.sif
-
-sudo singularity build poseidon2.sif poseidon2.def
---
+++ singularity/build_sif_and_deploy_to_MPISHH_cluster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm poseidon2.sif
+
+sudo singularity build poseidon2.sif poseidon2.def
+
+scp poseidon2.sif schmid@mpi-sdag1.sdag.ppj.shh.mpg.de:/projects1/singularity_scratch/cache/poseidon2.sif
