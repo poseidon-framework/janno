037f98ac4095cd86f7d5b7c06980a2202f125cd3
Clemens Schmid <clemens@nevrome.de>
2020-07-17 10:34:01 +0200

    and more restructering

---
+++ R/convert.R
@@ -0,0 +1,31 @@
+#' @rdname cli_modules
+#' @export
+convert_module <- function(output_format, input_package, output_directory, log_directory) {
+  # input check and prep
+  checkmate::assert_choice(output_format, choices = c("eigenstrat"))
+  checkmate::assert_directory_exists(input_package, access = "r")
+  checkmate::assert_directory_exists(log_directory, access = "rw")
+  #validate_module(input_package)
+  if (dir.exists(output_directory)) {
+    stop("output directory already exists")
+  } else {
+    dir.create(output_directory, recursive = T)
+  }
+  checkmate::assert_directory_exists(output_directory, access = "rw")
+  # start message
+  convert_start_message(output_format, input_package, output_directory, log_directory)
+  # select conversion submodule
+  if (output_format == "eigenstrat") {
+    convert_ped2eig(input_package, output_directory, log_directory)
+  } else {
+    stop("Unknown output format.")
+  }
+}
+
+convert_start_message <- function(output_format, input_package, output_directory, log_directory) {
+  cli::cli_h1("convert => Converts data in poseidon packages")
+  cli::cli_alert(paste0("Output format:\t", output_format))
+  cli::cli_alert(paste0("Input package:\t", input_package))
+  cli::cli_alert(paste0("Output directory:\t", output_directory))
+  cli::cli_alert(paste0("Log file directory:\t", log_directory))
+}
--- R/convert_ped2eig.R
+++ R/convert_ped2eig.R
@@ -1,32 +1,3 @@
-#' @rdname cli_modules
-#' @export
-convert_module <- function(output_format, input_package, output_directory, log_directory) {
-  # input check and prep
-  checkmate::assert_choice(output_format, choices = c("eigenstrat"))
-  checkmate::assert_directory_exists(input_package, access = "r")
-  checkmate::assert_directory_exists(log_directory, access = "rw")
-  #validate_module(input_package)
-  if (dir.exists(output_directory)) {
-    stop("output directory already exists")
-  } else {
-    dir.create(output_directory, recursive = T)
-  }
-  checkmate::assert_directory_exists(output_directory, access = "rw")
-  # start message
-  convert_start_message(output_format, input_package, output_directory, log_directory)
-  # select conversion submodule
-  if (output_format == "eigenstrat") {
-    convert_ped2eig(input_package, output_directory, log_directory)
-  } else {
-    stop("Unknown output format.")
-  }
-}
-
-convert_check_input <- function(output_format, input_package, output_directory, log_directory) {
-
-  
-}
-
 convert_ped2eig <- function(input_package, output_directory, log_directory) {
   cli::cli_alert_info("Converting plink files to eigenstrat format...")
   # copy janno file
@@ -88,12 +59,4 @@ convert_create_pedind_file <- function(fam_file, log_directory) {
   readr::write_delim(fam_table[,c(1:5, 1)], path = pedind_file, delim = " ", col_names = F)
   cli::cli_alert_success(pedind_file)
   return(pedind_file)
-}
-
-convert_start_message <- function(output_format, input_package, output_directory, log_directory) {
-  cli::cli_h1("convert => Converts data in poseidon packages")
-  cli::cli_alert(paste0("Output format:\t", output_format))
-  cli::cli_alert(paste0("Input package:\t", input_package))
-  cli::cli_alert(paste0("Output directory:\t", output_directory))
-  cli::cli_alert(paste0("Log file directory:\t", log_directory))
 }
