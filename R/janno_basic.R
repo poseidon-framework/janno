d5185b2d3a08f630480317ca69abe355a1b8dd86
Clemens Schmid <clemens@nevrome.de>
2020-07-29 17:36:38 +0200

    janno file reading without the critical steps

--- R/janno_basic.R
+++ R/janno_basic.R
@@ -28,18 +28,31 @@ check_if_all_columns_present <- function(x) {
 #' read_janno
 #'
 #' @param x character. Path to a .janno file
+#' @param validate logical. Should the janno file be validated upon reading
+#' @param to_janno logical. Should the read function transform the input file to a janno object
 #'
 #' @export
-read_janno <- function(x = "test_data/1_good_test_package/file1.janno") {
+read_janno <- function(
+  x = "test_data/1_good_test_package/file1.janno", 
+  validate = TRUE, 
+  to_janno = TRUE
+) {
   # input checks
   checkmate::assert_file_exists(x)
+  # validation
+  if (validate) {
   validation_result <- validate_janno(x)
-  if (validation_result == 1) {
-    stop("Input janno file has significant shortcomings and can't be loaded as a janno object in R.")
-  } else if (validation_result == 2) {
-    message("Input janno file has shortcomings, but loading is attempted anyway.")
+    if (validation_result == 1) {
+      stop("Input janno file has significant shortcomings and can't be loaded as a janno object in R.")
+    } else if (validation_result == 2) {
+      message("Input janno file has shortcomings, but loading is attempted anyway.")
+    }
   }
   # read file
-  readr::read_tsv(x, col_types = readr::cols(.default = readr::col_character()), na = "n/a") %>%
-    as.janno()
+  input_file <- readr::read_tsv(x, col_types = readr::cols(.default = readr::col_character()), na = "n/a") 
+  if (to_janno) {
+    as.janno(input_file)
+  } else {
+    input_file
+  }
 }
