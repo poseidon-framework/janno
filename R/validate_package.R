f346b8e5ab6b3923a0d3285f09f858eaeaf54371
Clemens Schmid <clemens@nevrome.de>
2020-07-29 17:14:12 +0200

    more easy to read package file check

--- R/validate_package.R
+++ R/validate_package.R
@@ -1,19 +1,52 @@
 validate_package <- function(input_package) {
+  # flag for less important conditions
+  everything_fine_flag <- TRUE
+  # beginn package check
   cli::cli_h2(input_package)
   # does it exist?
   if ( !checkmate::test_directory_exists(input_package) ) {
     cli::cli_alert_danger("The package directory does not exist")
     return(1)
   }
-  # validate POSEIDON.yml
-  POSEIDON_yml_file <- list.files(input_package, pattern = "POSEIDON\\.yml", full.names = T)
-  cli::cli_h3(POSEIDON_yml_file)
-  if ( !checkmate::test_string(POSEIDON_yml_file, min.chars = 1) ) {
-    cli::cli_alert_danger(
-      "Can't find POSEIDON.yml file"
-    )
+  cli::cli_h3("General package checks")
+  # collect file paths
+  POSEIDON_yml_file <- list.files(input_package, pattern = "POSEIDON.yml", full.names = T)
+  janno_file <- list.files(input_package, pattern = "\\.janno", full.names = T)
+  bed_file <- list.files(input_package, pattern = "\\.bed", full.names = T)
+  bim_file <- list.files(input_package, pattern = "\\.bim", full.names = T)
+  fam_file <- list.files(input_package, pattern = "\\.fam", full.names = T)
+  README_file <- list.files(input_package, pattern = "README.txt", full.names = T)
+  CHANGELOG_file <- list.files(input_package, pattern = "CHANGELOG.txt", full.names = T)
+  bib_file <- list.files(input_package, pattern = "LITERATURE.bib", full.names = T)
+  # does it contain the necessary files exactly once?
+  if ( 
+    !checkmate::test_string(POSEIDON_yml_file, min.chars = 1) | 
+    !checkmate::test_string(janno_file, min.chars = 1) | 
+    !checkmate::test_string(bed_file, min.chars = 1) | 
+    !checkmate::test_string(bim_file, min.chars = 1) | 
+    !checkmate::test_string(fam_file, min.chars = 1)
+  ) {
+    cli::cli_alert_danger(paste(
+      "One or multiple of the necessary files",
+      "(POSEIDON.yml, .janno, .bed, .bim, .fam)",
+      "are missing or occur multiple times"
+    ))
     return(1)
   }
+  # are other files present?
+  all_files <- list.files(input_package)
+  additional_files <- all_files[
+    !all_files %in% basename(
+      c(POSEIDON_yml_file, janno_file, bed_file, bim_file, fam_file, README_file, CHANGELOG_file, bib_file))
+  ]
+  if (length(additional_files) > 0) {
+    cli::cli_alert_warning(paste(
+      "There are supplementary files present in this package:", 
+      paste(additional_files, collapse = ", ")
+    ))
+  }
+  # validate POSEIDON.yml
+  cli::cli_h3(POSEIDON_yml_file)
   if ( !can_POSEIDON_yml_be_read(POSEIDON_yml_file) ) {
     return(1)
   }
@@ -21,38 +54,8 @@ validate_package <- function(input_package) {
   if ( !has_POSEIDON_yml_the_necessary_elements(names(pyml)) ) {
     return(1)
   }
-  # flag for less important conditions
-  everything_fine_flag <- TRUE
   if ( !validate_POSEIDON_yml(pyml, input_package) ) {
     everything_fine_flag <- FALSE
-  }
-  cli::cli_h3("General package checks")
-  # does it contain the other necessary files exactly once?
-  janno_file <- list.files(input_package, pattern = "\\.janno")
-  bed_file <- list.files(input_package, pattern = "\\.bed")
-  bim_file <- list.files(input_package, pattern = "\\.bim")
-  fam_file <- list.files(input_package, pattern = "\\.fam")
-  if ( 
-    !checkmate::test_string(janno_file, min.chars = 1) | 
-    !checkmate::test_string(bed_file, min.chars = 1) | 
-    !checkmate::test_string(bim_file, min.chars = 1) | 
-    !checkmate::test_string(fam_file, min.chars = 1) 
-  ) {
-    cli::cli_alert_danger(
-      "One or multiple of the necessary files (.bed, .bim, .fam and .janno) are missing duplicate multiple times"
-    )
-    return(1)
-  }
-  # are other files present?
-  all_files <- list.files(input_package)
-  additional_files <- all_files[
-    !all_files %in% c(basename(POSEIDON_yml_file), janno_file, bed_file, bim_file, fam_file)
-    ]
-  if (length(additional_files) > 0) {
-    cli::cli_alert_warning(paste(
-      "There are supplementary files present in this package:", 
-      paste(additional_files, collapse = ", ")
-    ))
   }
   # check .janno file
   janno_error_code <- validate_janno(list.files(input_package, pattern = "\\.janno", full.names = T))
