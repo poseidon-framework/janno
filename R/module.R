39a5224393fd02692cf13a334097f12f410565fb
Clemens Schmid <clemens@nevrome.de>
2020-07-31 12:38:41 +0200

    added missing documentation

--- R/module.R
+++ R/module.R
@@ -1,6 +1,7 @@
 #' poseidon2 command line interface module functions
 #'
 #' @param input_file character. Path to input file
+#' @param filter_file character. Path to filter file
 #' @param output_directory character. Path to output directory
 #' @param log_directory character. Path to directory for log files and temporary data
 #' @param output_format character. Output format of conversion (e.g. eigenstrat)
