cc4d8a4ee7f4fa04056646a239424ed56f174dbf
Clemens Schmid <clemens@nevrome.de>
2020-10-18 11:42:20 +0200

    restructured janno basic class functions

--- R/janno_basic.R
+++ R/janno_basic.R
@@ -1,8 +1,19 @@
-#' as.janno
+#' @name janno
+#' @title \strong{janno}
 #'
-#' @param x data.frame. Object to be converted to a janno object
+#' @description ...
+#'
+#' @param x an object
+#' @param file character. Path to a .janno file
+#' @param validate logical. Should the janno file be validated upon reading
+#' @param to_janno logical. Should the read function transform the input file to a janno object
 #' @param ... further arguments passed to or from other methods
-#' 
+#'
+#' @rdname janno
+#'
+NULL
+
+#' @rdname janno
 #' @export
 as.janno <- function(x, ...) {
   
@@ -25,23 +36,18 @@ check_if_all_columns_present <- function(x) {
   }
 }
 
-#' read_janno
-#'
-#' @param x character. Path to a .janno file
-#' @param validate logical. Should the janno file be validated upon reading
-#' @param to_janno logical. Should the read function transform the input file to a janno object
-#'
+#' @rdname janno
 #' @export
 read_janno <- function(
-  x = "test_data/1_good_test_package/file1.janno", 
+  file = "test_data/1_good_test_package/file1.janno", 
   validate = TRUE, 
   to_janno = TRUE
 ) {
   # input checks
-  checkmate::assert_file_exists(x)
+  checkmate::assert_file_exists(file)
   # validation
   if (validate) {
-  validation_result <- validate_janno(x)
+  validation_result <- validate_janno(file)
     if (validation_result == 1) {
       stop("Input janno file has significant shortcomings and can't be loaded as a janno object in R.")
     } else if (validation_result == 2) {
@@ -49,10 +55,26 @@ read_janno <- function(
     }
   }
   # read file
-  input_file <- readr::read_tsv(x, col_types = readr::cols(.default = readr::col_character()), na = "n/a") 
+  input_file <- readr::read_tsv(file, col_types = readr::cols(.default = readr::col_character()), na = "n/a") 
   if (to_janno) {
     as.janno(input_file)
   } else {
     input_file
   }
+}
+
+#' @rdname janno
+#' @export
+format.janno <- function(x, ...) {
+  out_str <- list()
+  out_str$header <- paste0("janno")
+  return_value <- paste(out_str, collapse = "\n", sep = "")
+  invisible(return_value)
+}
+
+#' @rdname janno
+#' @export
+print.janno <- function(x, ...) {
+  # own format function
+  cat(format(x, ...), "\n\n")
 }
