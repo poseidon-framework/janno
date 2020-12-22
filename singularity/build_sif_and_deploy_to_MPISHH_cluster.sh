a1273ac049737096f06dc0c3f64a2a6f36e4ee45
Clemens Schmid <clemens@nevrome.de>
2020-12-22 13:01:22 +0100

    deleted cluster CLI deployment scripts

--- singularity/build_sif_and_deploy_to_MPISHH_cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -f poseidon2.sif
-
-sudo singularity build poseidon2.sif poseidon2.def
-
-scp poseidon2.sif schmid@mpi-sdag1.sdag.ppj.shh.mpg.de:/projects1/singularity_scratch/cache/poseidon2.sif
--- singularity/poseidon2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-singularity exec --bind=/projects1 /projects1/singularity_scratch/cache/poseidon2.sif /poseidon2.R "$@"
-
