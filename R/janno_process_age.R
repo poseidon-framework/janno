5d27389118c3f06aa42302c20114624e07f40b7f
Clemens Schmid <clemens@nevrome.de>
2020-12-30 15:51:40 +0100

    reactivated two-sigma range determination as part of the age processing

--- R/janno_process_age.R
+++ R/janno_process_age.R
@@ -3,9 +3,11 @@
 #'
 #' @description Process all valid dates in a \strong{janno} table
 #' 
-#' @param x an object of class janno
-#' @param choices character vector. Which output columns should be added?
-#' @param n integer. If "Date_BC_AD_Sample" in choices, then how many samples should be drawn? Default: 100
+#' @param x an object of class \strong{janno}
+#' @param choices character vector. Which output columns should be added? 
+#' By default all possible columns are added.
+#' @param n integer. If "Date_BC_AD_Sample" is in \code{choices}, 
+#' then how many samples should be drawn?
 #' @param ... further arguments passed to or from other methods
 #' 
 #' @rdname process_age
@@ -120,7 +122,7 @@ contextual_date_uniform <- function(startbcad, stopbcad) {
     tibble::tibble(
       age = startbcad[i]:stopbcad[i],
       sum_dens = 1/(length(startbcad[i]:stopbcad[i])),
-      #two_sigma = TRUE,
+      two_sigma = TRUE,
       center = .data[["age"]] == round(mean(c(startbcad[i]:stopbcad[i])))
     )
   })
@@ -130,7 +132,7 @@ contextual_date_uniform <- function(startbcad, stopbcad) {
 sumcal_list_of_multiple_dates <- function(individual_id_list, age_list, err_list) {
   
   bol <- 1950 # c14 reference zero
-  #threshold <- (1 - 0.9545) / 2 # 2sigma range probability threshold
+  threshold <- (1 - 0.9545) / 2 # 2sigma range probability threshold
   
   pb <- progress::progress_bar$new(total = length(age_list))
   
@@ -192,12 +194,12 @@ sumcal_list_of_multiple_dates <- function(individual_id_list, age_list, err_list
       )
     
     a <- cumsum(sum_density_table$sum_dens) # cumulated density
-    #bottom <- sum_density_table$age[max(which(a <= threshold))]
-    #top <- sum_density_table$age[min(which(a > 1 - threshold))]
+    bottom <- sum_density_table$age[max(which(a <= threshold))]
+    top <- sum_density_table$age[min(which(a > 1 - threshold))]
     center <- sum_density_table$age[which.min(abs(a - 0.5))]
     
     result_table <- sum_density_table %>% dplyr::mutate(
-      #two_sigma = .data[["age"]] >= bottom & .data[["age"]] <= top,
+      two_sigma = .data[["age"]] >= bottom & .data[["age"]] <= top,
       center = .data[["age"]] == center
     )
     
