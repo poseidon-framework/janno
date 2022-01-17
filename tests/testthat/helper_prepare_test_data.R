74e5f31c50c370e53154c2a64f56bfaff552942c
Clemens Schmid <clemens@nevrome.de>
2022-01-17 12:55:50 +0100

    started to update the tests beyond the test data

--- tests/testthat/helper_prepare_test_data.R
+++ tests/testthat/helper_prepare_test_data.R
@@ -1,8 +1,12 @@
 path_janno_borked_full <- "poseidon_test_data/testJannoFiles/borked_full.janno"
 path_janno_borked_partial <- "poseidon_test_data/testJannoFiles/borked_partial.janno"
-path_janno_borked_wrong_name <- "poseidon_test_data/testJannoFiles/borked_wrong_name.janno"
 path_janno_minimal_full <- "poseidon_test_data/testJannoFiles/minimal_full.janno"
 path_janno_minimal_partial <- "poseidon_test_data/testJannoFiles/minimal_partial.janno"
 path_janno_minimal_partial_extra_columns <- "poseidon_test_data/testJannoFiles/minimal_partial_extra_columns.janno"
 path_janno_normal_full <- "poseidon_test_data/testJannoFiles/normal_full.janno"
 path_janno_normal_partial <- "poseidon_test_data/testJannoFiles/normal_partial.janno"
+
+path_janno_borked_wrong_name <- "poseidon_test_data/testJannoFiles/specificallyBorked/borked_wrong_name.janno"
+path_janno_borked_dating <- "poseidon_test_data/testJannoFiles/specificallyBorked/borked_dating.janno"
+path_janno_borked_relations <- "poseidon_test_data/testJannoFiles/specificallyBorked/borked_relations.janno"
+path_janno_borked_contamination <- "poseidon_test_data/testJannoFiles/specificallyBorked/borked_contamination.janno"
--- tests/testthat/test-validate_janno.R
+++ tests/testthat/test-validate_janno.R
@@ -1,45 +1,45 @@
-test_that("validating broken_full.janno works", {
+test_that("validating broken_full.janno works (list of errors)", {
   expect_snapshot_value(
     suppressMessages(validate_janno(path_janno_borked_full)),
     style = c("json2")
   )
 })
 
-test_that("validating broken_partial.janno works", {
+test_that("validating broken_partial.janno works (list of errors)", {
   expect_snapshot_value(
     suppressMessages(validate_janno(path_janno_borked_partial)),
     style = c("json2")
   )
 })
 
-test_that("validating borked_wrong_name.janno fails", {
+test_that("validating borked_wrong_name.janno fails (fails to load)", {
   expect_error(
     suppressMessages(validate_janno(path_janno_borked_wrong_name))
   )
 })
 
-test_that("validating minimal_full.janno works", {
+test_that("validating minimal_full.janno works (no errors)", {
   expect_snapshot_value(
     suppressMessages(validate_janno(path_janno_minimal_full)),
     style = c("json2")
   )
 })
 
-test_that("validating minimal_partial.janno works", {
+test_that("validating minimal_partial.janno works (no errors)", {
   expect_snapshot_value(
     suppressMessages(validate_janno(path_janno_minimal_partial)),
     style = c("json2")
   )
 })
 
-test_that("validating minimal_partial_extra_columns.janno works", {
+test_that("validating minimal_partial_extra_columns.janno works (no errors)", {
   expect_snapshot_value(
     suppressMessages(validate_janno(path_janno_minimal_partial_extra_columns)),
     style = c("json2")
   )
 })
 
-test_that("validating normal_full.janno works", {
+test_that("validating normal_full.janno works (no errors)", {
   expect_snapshot_value(
     suppressMessages(validate_janno(path_janno_normal_full)),
     style = c("json2")
