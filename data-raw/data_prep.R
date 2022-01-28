61fcb5f7b3d21178eac82f441b566a912b207e82
Clemens Schmid <clemens@nevrome.de>
2022-01-28 15:09:18 +0100

    version bump

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -1,5 +1,5 @@
 janno_columns <- readr::read_tsv(
-  "https://raw.githubusercontent.com/poseidon-framework/poseidon2-schema/janno_column_names_and_order/janno_columns.tsv" # this is only in a PR
+  "https://raw.githubusercontent.com/poseidon-framework/poseidon2-schema/master/janno_columns.tsv"
 )
 
 # column names
