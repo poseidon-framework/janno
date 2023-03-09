40c994ef7e9216d5d9f4640c4f9831d191c4594d
Clemens Schmid <clemens@nevrome.de>
2023-03-09 14:29:19 +0100

    cleaned out unused helper functions

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,22 +1,3 @@
-get_genetic_data_type <- function(input_package) {
-  files_in_package <- list.files(input_package)
-  file_types_in_package <- unique(get_extension(files_in_package))
-  res <- c()
-  # plink_binary
-  if (all(c("bed", "bim", "fam") %in% file_types_in_package)) { 
-    res <- append(res, "plink_binary") 
-  }
-  # eigenstrat
-  if (all(c("geno", "snp", "ind") %in% file_types_in_package)) { 
-    res <- append(res, "eigenstrat") 
-  }
-  return(res)
-}
-
-get_extension <- function(files){ 
-  sapply(strsplit(basename(files), split="\\."), function(x) {x[-1]})
-} 
-
 get_janno_file_paths <- function(path) {
   lapply(
     path, function(x) {
